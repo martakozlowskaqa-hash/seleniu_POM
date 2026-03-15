4deb281b4cc8f95a22cae3f2095ff37a535572e0
martakozlowskaqa-hash <martakozlowska.qa@gmail.com>
2026-03-15 10:12:47 +0100

    add test_data files and utils

---
+++ test_data/registration_data_generator.py
@@ -0,0 +1,16 @@
+from faker import Faker
+from utils.custom_types import Gender
+import random
+
+class RegistrationDataGenerator:
+    def __init__(self):
+        self.GENDER = random.choice([Gender.MALE, Gender.FEMALE])
+        self.fake = Faker("pl_PL")
+        if self.GENDER == Gender.FEMALE:
+            self.FIRST_NAME = self.fake.first_name_female()
+        else:
+            self.FIRST_NAME = self.fake.first_name_male()
+
+d = RegistrationDataGenerator()
+print(d.FIRST_NAME)
+print(d.GENDER)
