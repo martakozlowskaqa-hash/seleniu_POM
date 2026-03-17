0b3aac64dbdf607eafb3ad2aec8a952e8ee6faf0
martakozlowskaqa-hash <martakozlowska.qa@gmail.com>
2026-03-17 19:48:36 +0100

    add ddt

--- tests/registration_test.py
+++ tests/registration_test.py
@@ -1,8 +1,11 @@
 from pages.home_page import HomePage
 from tests.base_test import BaseTest
 from time import sleep
-from test_data.registration_data_generator import RegistrationDataGenerator
+from test_data.registration_data import RegistrationDataGenerator
+from ddt import ddt, data, unpack
+import test_data.registration_data
 
+@ddt
 class RegistrationTest(BaseTest):
     def setUp(self):
         super().setUp()
@@ -11,13 +14,15 @@ class RegistrationTest(BaseTest):
         self.authentication_page.enter_create_account_email(self.data.EMAIL)
         self.create_account_page = self.authentication_page.click_create_account_btn()
 
-    def testNoLastname(self):
+    @data(*test_data.registration_data.get_csv_data("test_data/registration.csv"))
+    @unpack
+    def testNoLastname(self, gender, firstname, lastname, email, password, day, month, year):
         self.create_account_page.choose_gender(self.data.GENDER)
-        self.create_account_page.enter_first_name(self.data.FIRST_NAME)
+        self.create_account_page.enter_first_name(firstname)
         # print(self.create_account_page.get_entered_email())
         # print(self.data.EMAIL)
         self.assertEqual(self.data.EMAIL, self.create_account_page.get_entered_email())
-        self.create_account_page.enter_password(self.data.PASSWORD)
+        self.create_account_page.enter_password(self.data.password)
         self.create_account_page.select_date_of_birth(self.data.DATE_OF_BIRTH)
         self.create_account_page.click_register_button()
         expected_number_of_errors = "There is 1 error"
